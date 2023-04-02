89cd5c4a57da1ac034292811847c86c9c31f0f86
Muhammed <coolzyte@gmail.com>
2023-04-02 22:03:28 +0100

    update the svg icons

---
+++ src/components/AboutIcon.js
@@ -0,0 +1,28 @@
+function AboutIcon() {
+  return (
+    <svg
+      width="60"
+      height="120"
+      viewBox="0 0 60 120"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle cx="60" cy="60" r="60" fill="url(#paint0_radial_18:24)" />
+      <defs>
+        <radialGradient
+          id="paint0_radial_18:24"
+          cx="0"
+          cy="0"
+          r="1"
+          gradientUnits="userSpaceOnUse"
+          gradientTransform="translate(60) rotate(90) scale(120)"
+        >
+          <stop stop-color="white" />
+          <stop offset="0.569" stop-color="#F0F4FD" />
+          <stop offset="0.993" stop-color="#D9E0F0" />
+        </radialGradient>
+      </defs>
+    </svg>
+  );
+}
+export default AboutIcon;
